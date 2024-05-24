e98dccc2f280046006958c95fe93eccae9bceddb
Minos_qg35 <dimamin2802@gmail.com>
2024-05-24 15:43:05 +0300

    Initial commit

---
+++ src/main/java/Course.java
@@ -0,0 +1,110 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //это auto_increment    private int id;
+    private int id;
+    private String name;
+    private int duration;
+    @Enumerated(EnumType.STRING)
+
+    @Column(name = "type", columnDefinition = "enum")
+    private CourseType courseType;
+    private String description;
+    @Column(name = "students_count")
+    private int studentsCount;
+    private int price;
+    @Column(name = "price_per_hour")
+    private float pricePerHour;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "subscriptions",
+            joinColumns = {@JoinColumn(name = "student_id")},
+            inverseJoinColumns = {@JoinColumn(name = "course_id")})
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(CourseType courseType) {
+        this.courseType = courseType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStudentsCount() {
+        return studentsCount;
+    }
+
+    public void setStudentsCount(int studentsCount) {
+        this.studentsCount = studentsCount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public float getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(float pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", duration=" + duration +
+                ", courseType=" + courseType +
+                ", description='" + description + '\'' +
+                ", teacher=" + 
+                ", studentsCount=" + studentsCount +
+                ", price=" + price +
+                ", pricePerHour=" + pricePerHour +
+                '}';
+    }
+}
